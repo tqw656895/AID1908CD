e94ecd32275e552f0971d088b637d8e4e1b8e1d0
tqw656895 <56908561+tqw656895@users.noreply.github.com>
2019-10-23 16:30:32 +0800

    Update 123.py
    change 123.py

--- 123.py
+++ 123.py
@@ -1,5 +1,8 @@
-try:
-    f = open("hello.py","a",buffering=-1)
-    print(f)
-except Exception as e:
-    print(e)
+# try:
+#     f = open("hello.py","a",buffering=-1)
+#     print(f)
+# except Exception as e:
+#     print(e)
+def fun():
+    print('hhh')
+print "aaaa"
